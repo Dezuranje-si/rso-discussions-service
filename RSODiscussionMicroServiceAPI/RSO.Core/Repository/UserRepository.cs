e2714850b2b0c407e9075e99df999f8df3949919
shanji97 <aleksander.kovac97@hotmail.com>
2024-01-09 22:42:08 +0100

    Initiated project.

---
+++ RSODiscussionMicroServiceAPI/RSO.Core/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RSO.Core.UserModels;
+using UserServiceRSO.Repository;
+
+namespace RSO.Core.Repository;
+
+public class UserRepository : GenericRepository<User>, IUserRepository
+{
+    public UserRepository(UserServicesRSOContext context) : base(context) { }
+
+    ///<inheritdoc/>
+    public async Task<bool> UserExistsByUserNameOrEmailOrPasswordAsync(string emailOrUsername, string password) => await _context.User.AnyAsync(u => (u.UserName.Equals(emailOrUsername) || u.UserEmail.Equals(emailOrUsername)) && u.UserPassword.Equals(password));
+
+    ///<inheritdoc/>
+    public async Task<bool> UsernameOrEmailAlreadyTakenAsync(string userName, string email) => await _context.User.AnyAsync(u => u.UserName.Equals(userName) || u.UserEmail.Equals(email));
+
+    ///<inheritdoc/>
+    public async Task<User> GetByUsernameOrEmailAndPasswordAsync(string emailOrUsername, string password) => await _context.User.Where(u => (u.UserName.Equals(emailOrUsername) || u.UserEmail.Equals(emailOrUsername)) && u.UserPassword.Equals(password)).FirstOrDefaultAsync();
+
+    ///<inheritdoc/>
+    public async Task DeleteUserAsync(User user) => await _context.User.Where(User => User.UserId == user.UserId).ExecuteDeleteAsync();
+
+    ///<inheritdoc/>
+    public async Task UpdateUsersNameAsync(User user) => await _context.User.Where(User => User.UserId == user.UserId).ExecuteUpdateAsync(users => users.SetProperty(u => u.UserName,user.UserName));
+}
---
+++ RSODiscussionMicroServiceAPI/RSODiscussionMicroServiceAPI/Program.cs
@@ -0,0 +1,100 @@
+using Carter;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using RSO.Core.BL;
+using RSO.Core.Configurations;
+using RSO.Core.Repository;
+using RSO.Core.UserModels;
+using System.Text;
+using UserServiceRSO.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register the IOptions object.
+builder.Services.AddOptions<DiscussionServicesSettingsConfiguration>()
+    .BindConfiguration("DiscussionServicesSettingsConfiguration");
+//Explicitly register the settings objects by delegating to the IOptions object.
+builder.Services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<DiscussionServicesSettingsConfiguration>>().Value);
+
+// Database connection
+builder.Services.AddDbContext<DiscussionServicesRSOContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DiscussionServicesRSOdB")));
+
+// Razor pages
+builder.Services.AddRazorPages();
+
+// Lazy cache
+builder.Services.AddLazyCache();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDiscussionRepository, DiscussionRepository>(); //In each microservice a different repository/serice is inlcuded. If more tables are needed add more repos related to the microservice.
+builder.Services.AddScoped<IDiscussionLogic, DiscussionLogic>(); //In each microservice a different repository/serice is inlcuded.*/
+
+// JWT
+var jwtSecurityConfig = builder.Configuration.GetSection("JwtSecurityTokenConfiguration").Get<JwtSecurityTokenConfiguration>();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecurityConfig.SecretKey)),
+            ValidIssuer = jwtSecurityConfig.Issuer,
+            ValidAudience = jwtSecurityConfig.Audience
+        };
+    });
+
+//Carter
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddCarter();
+// Add services to the container.
+builder.Services.AddAuthorization();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddOpenApiDocument(options =>
+{
+    options.PostProcess = document =>
+    {
+        document.Info = new()
+        {
+            Version = "v1",
+            Title = "Discussions microservices API",
+            Description = "DiscussionLogic microservices API endpoints",
+            TermsOfService = "Lol.",
+            Contact = new()
+            {
+                Name = "Aleksander Kovac & Urban Poljsak",
+                Url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            }
+        };
+    };
+    options.UseControllerSummaryAsTagDescription = true;
+});
+// APP.
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+// Carter
+app.MapCarter();
+app.UseOpenApi();
+app.UseSwaggerUi3(options =>
+{
+    options.Path = "/openapi";
+    options.TagsSorter = "Discussions";
+});
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllers();
+});
